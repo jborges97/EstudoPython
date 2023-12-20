a8b2974ea6643586f1b0b4265a4b85691bd0bf83
jborges97 <julionastasi@gmail.com>
2023-12-20 20:35:45 -0300

    first commit

---
+++ PI.py
@@ -0,0 +1,10 @@
+import math
+
+# Obtendo o valor de pi
+pi = math.pi
+
+# Arredondando para duas casas decimais
+pi_arredondado = round(pi, 2)
+
+# Imprimindo a frase
+print(f"O valor arredondado de pi é: {pi_arredondado}")
---
+++ nome.py
@@ -0,0 +1,4 @@
+nome = input('Digite seu nome: ')
+idade = input('Digite a sua idade: ')
+
+print(f'Meu nome é {nome} e tenho {idade} anos')
