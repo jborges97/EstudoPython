48518a1a505f764408c5c4913ed516b9a418abf6
jborges97 <julionastasi@gmail.com>
2023-12-21 17:08:20 -0300

    Melhorando o codigo com int

--- app.py
+++ app.py
@@ -8,13 +8,13 @@ print("""
 ╚═════╝░╚═╝░░╚═╝╚═════╝░░╚════╝░╚═╝░░╚═╝  ╚══════╝╚═╝░░╚═╝╚═╝░░░░░╚═╝░░╚═╝╚══════╝╚═════╝░╚═════╝░  
 """)
 
-print('1. cadastrar restaurante')
+print('1. Cadastrar restaurante')
 print('2. Listar restaurante')
 print('3. Ativar restaurante')
 print('4. Sair\n')
 
 
-opcao_escolhida = input('Escolha uma opção: ')
+opcao_escolhida = int(input('Escolha uma opção: '))
 
 if opcao_escolhida == 1:
     print('Cadastrar restaurante')
