95ea39e2a0dcc149d5fb7b0966bbef0c7592aae5
jborges97 <julionastasi@gmail.com>
2023-12-21 15:38:13 -0300

    Atualização programa

--- app.py
+++ app.py
@@ -15,6 +15,14 @@ print('4. Sair\n')
 
 
 opcao_escolhida = input('Escolha uma opção: ')
-print(f'Você escolheu a opção {opcao_escolhida}')
+
+if opcao_escolhida == 1:
+    print('Cadastrar restaurante')
+elif opcao_escolhida == 2:
+    print('Listar restaurantes')
+elif opcao_escolhida == 3:
+    print ('Ativar restaurantes')
+else:
+    print('Encerrando o programa')            
 
 
