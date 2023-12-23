469ff99845f38aad50e7b896e0b7bc3c8da500c6
jborges97 <julionastasi@gmail.com>
2023-12-23 17:56:23 -0300

    Exercicio sobre Variaveis Alura

---
+++ ExCondicionais.py
@@ -0,0 +1,40 @@
+numero = int(input("Digite um numero: "))
+if numero % 2 == 0:
+    print("O número é par.")
+else:
+    print("O número é ímpar.")
+
+idade = int(input("Digite sua idade: "))
+if 0 < idade <= 12:
+    print("Criança")
+elif 12 < idade <= 18:
+    print("Adolescente")
+elif idade > 18:
+    print("Adulto")
+else: 
+    print("Valor inválido!")
+
+usuario_correto = "alura"
+senha_correta = "alura123"
+
+usuario = input("Digite o nome de usuário: ")
+senha = input("Digite a senha: ")
+
+if usuario == usuario_correto and senha == senha_correta:
+    print("Login bem sucedido!")
+else:
+    print("Credenciais inválidas. Tente novamente.")
+
+x = float(input("Digite a coordenada x: "))
+y = float(input("Digite a coordenada y: "))
+
+if x > 0 and y > 0:
+    print("O ponto está no primeiro quadrante.")
+elif x < 0 and y > 0:
+    print("O ponto está no segundo quadrante.")
+elif x < 0 and y < 0:
+    print("O ponto está no terceiro quadrante.")
+elif x > 0 and y < 0:
+    print("O ponto está no quarto quadrante.")
+else:
+    print("O ponto está sobre um eixo ou na origem.")
